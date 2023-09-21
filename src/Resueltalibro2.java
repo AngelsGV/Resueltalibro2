42259bdcadef734c1b38e8984b6d9a35bfe34a4e
AngelsGV <angels.not99@gmail.com>
2023-09-21 23:08:57 +0200

    Internal commit

---
+++ src/Resueltalibro2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Resueltalibro2{
+    public static void main (String[] args) {
+        int num;
+        System.out.print("Introduce un número:");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        System.out.println("Ha esctrito:" + num);
+
+    }
+}
